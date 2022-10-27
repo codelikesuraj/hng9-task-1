bef095e2219ac75579362150e9a8f90ada9d1edb
Abdulbaki Adabara Suraj <codelikesuraj@gmail.com>
2022-10-27 22:10:06 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+header('Content-type: application/json');
+
+return json_encode([
+  "slackUsername" => "codelikesuraj",
+  "backend" => true,
+  "age" => 22,
+  "bio" => "Backend developer"
+]);
