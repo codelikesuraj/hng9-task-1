c4a8b76b83ad412d5de131ced0f28aaa9a19fa83
Abdulbaki Adabara Suraj <codelikesuraj@gmail.com>
2022-11-04 08:04:50 +0100

    detect json and form requests

--- stage-2-task/index.php
+++ stage-2-task/index.php
@@ -4,34 +4,50 @@ header('Content-type: application/json');
 
 $request_method = strtolower($_SERVER['REQUEST_METHOD']);
 
+function check_fields(array $keys, array $array)
+{
+  foreach($keys as $key)
+  {
+    if(!array_key_exists($key, $array)){
+      return false;
+    }
+  }
+  return true;
+}
+
 if ($request_method == 'post') {
-  $data = json_decode(file_get_contents('php://input'));
+  if(isset($_POST) && check_fields(['operation_type', 'x', 'y'], $_POST))
+  {
+    $data = $_POST;
+  } else {
+    $data = (array)json_decode(file_get_contents('php://input'));
+  }
 
   // check operand
-  if (!isset($data->operation_type)) {
+  if (!isset($data['operation_type'])) {
     echo json_encode(['error' => 'invalid operation type']);
     exit();
   }
 
-  // if (!in_array(strtolower($data->operation_type), ['addition', 'subtraction', 'multiplication'])) {
-  //   echo json_encode(['error' => 'invalid operation type']);
-  //   exit();
-  // }
+  if (!in_array(strtolower($data['operation_type']), ['addition', 'subtraction', 'multiplication'])) {
+    echo json_encode(['error' => 'invalid operation type']);
+    exit();
+  }
 
   // check if values are set
-  if (!isset($data->x) || empty(trim($data->x))){
+  if (!isset($data['x']) || empty(trim($data['x'])) || !is_numeric(trim($data['x']))) {
     echo json_encode(['error' => 'invalid value for x']);
     exit();
   }
-  if (!isset($data->y) || empty(trim($data->y))){
+  if (!isset($data['y']) || empty(trim($data['y'])) || !is_numeric(trim($data['y']))) {
     echo json_encode(['error' => 'invalid value for y']);
     exit();
   }
 
   $result = 0;
-  $operation = $data->operation_type;
-  $x = intval(trim($data->x));
-  $y = intval(trim($data->y));
+  $operation = $data['operation_type'];
+  $x = intval($data['x']);
+  $y = intval($data['y']);
 
   switch ($operation) {
     case 'addition':
@@ -43,7 +59,7 @@ if ($request_method == 'post') {
     case 'multiplication':
       $result = $x * $y;
       break;
-    
+
     default:
       $result = $x + $y;
       break;
